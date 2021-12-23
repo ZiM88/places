6b6c81feec52721d8a0a4e27d3ab7b9653bfd9e7
ZiM88 <renegadez@tut.by>
2021-12-23 15:47:44 +0300

    first commit

---
+++ app/src/main/java/com/example/places/activities/HappyPlaceDetailActivity.kt
@@ -0,0 +1,48 @@
+package com.example.places.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.places.R
+import com.example.places.activities.MapActivity
+import com.example.places.models.HappyPlaceModel
+import kotlinx.android.synthetic.main.activity_happy_place_detail.*
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy_place_detail)
+
+        var happyPlaceDetailModel: HappyPlaceModel? = null
+
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)) {
+            happyPlaceDetailModel =
+                    intent.getSerializableExtra(MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel
+        }
+
+        if (happyPlaceDetailModel != null) {
+
+            setSupportActionBar(toolbar_happy_place_detail)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = happyPlaceDetailModel.title
+
+            toolbar_happy_place_detail.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            iv_place_image.setImageURI(Uri.parse(happyPlaceDetailModel.image))
+            tv_description.text = happyPlaceDetailModel.description
+            tv_location.text = happyPlaceDetailModel.location
+        }
+
+        btn_view_on_map.setOnClickListener {
+            val intent = Intent(this@HappyPlaceDetailActivity, MapActivity::class.java)
+            intent.putExtra(MainActivity.EXTRA_PLACE_DETAILS, happyPlaceDetailModel)
+            startActivity(intent)
+        }
+    }
+}
